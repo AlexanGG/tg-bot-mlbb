54b29a4908de217b217fa5f9efacbbb4b145ece5
alexandr <shurik.garilov.2000@mail.ru>
2025-02-11 10:59:59 +0300

    Create Seders

---
+++ database/seeders/HeroSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Hero;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class HeroSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Hero::create(['name' => 'Julian', 'image' => 'julian.png']);
+        Hero::create(['name' => 'Joy', 'image' => 'joy.png']);
+        // Добавь другие героев
+    }
+}
