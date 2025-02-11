062dc0d1d80402557c80dfe4343e7e6a7afd8fdb
alexandr <shurik.garilov.2000@mail.ru>
2025-02-11 13:29:00 +0300

    Edit + Create DataBase Seeder

--- database/migrations/2025_02_11_065926_create_tier_list_table.php
+++ database/migrations/2025_02_11_065926_create_tier_list_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tier_list', function (Blueprint $table) {
+        Schema::create('tiers', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
             $table->timestamps();
@@ -23,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tier_list');
+        Schema::dropIfExists('tiers');
     }
 };
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        // Вызов всех сидеров
+        $this->call([
+            HeroSeeder::class,
+            RoleSeeder::class,
+            TierListSeeder::class,
+            BuildSeeder::class,
+            BuildItemSeeder::class,
+        ]);
     }
 }
