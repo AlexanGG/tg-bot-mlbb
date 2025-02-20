8121197117756d404b53ac7191f26f40e0b80b0c
alexandr <shurik.garilov.2000@mail.ru>
2025-02-20 17:57:54 +0300

    Абстракция

--- app/Bot/Interfaces/CommandInterface.php
+++ app/Bot/Interfaces/CommandInterface.php
@@ -4,5 +4,10 @@ namespace App\Bot\Interfaces;
 
 interface CommandInterface
 {
-
+    /**
+     * Обработка входящего сообщения от Telegram.
+     *
+     * @param array $message
+     */
+    public function handle(array $message): void;
 }
