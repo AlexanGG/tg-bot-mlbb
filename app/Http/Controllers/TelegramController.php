6b67828b78252b6ff8f28d95fd166af7d159b9f6
alexandr <shurik.garilov.2000@mail.ru>
2025-02-20 17:59:56 +0300

    Контроллер для обработки webhook

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -2,7 +2,23 @@
 
 namespace App\Http\Controllers;
 
-class TelegramController
-{
+use App\Bot\Core\TelegramBot;
+use App\Bot\Services\TelegramService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
+class TelegramController extends Controller
+{
+    public function handle(Request $request): JsonResponse
+    {
+        Log::info('Telegram update received:', $request->all());
+
+        $botToken = config('telegram.bot_token');
+        $telegramService = new TelegramService($botToken);
+        $bot = new TelegramBot($telegramService);
+        $bot->run();
+
+        return response()->json(['status' => 'ok']);
+    }
 }
