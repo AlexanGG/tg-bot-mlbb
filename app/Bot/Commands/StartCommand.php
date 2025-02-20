330afe991db781e96efd95c5dd4118ad123c6b67
alexandr <shurik.garilov.2000@mail.ru>
2025-02-20 17:55:30 +0300

    Старт команды в тг-боте

--- app/Bot/Commands/StartCommand.php
+++ app/Bot/Commands/StartCommand.php
@@ -1,8 +1,23 @@
 <?php
-
 namespace App\Bot\Commands;
 
-class StartCommand
-{
+use App\Bot\Interfaces\CommandInterface;
+use App\Bot\Services\TelegramService;
 
+class StartCommand implements CommandInterface
+{
+    private TelegramService $telegramService;
+
+    public function __construct(TelegramService $telegramService)
+    {
+        $this->telegramService = $telegramService;
+    }
+
+    public function handle(array $message): void
+    {
+        $chatId = $message['chat']['id'];
+        $text = "Привет! Добро пожаловать в бота по МЛББ.\n" .
+            "Используйте /meta для получения меты и /build для получения сборки героя.";
+        $this->telegramService->sendMessage($chatId, $text);
+    }
 }
