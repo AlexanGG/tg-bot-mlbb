54adfb0e4726051a8c2093237c69701bd5b25fbb
alexandr <shurik.garilov.2000@mail.ru>
2025-02-20 17:55:07 +0300

    Логика для меты гереов

--- app/Bot/Commands/MetaCommand.php
+++ app/Bot/Commands/MetaCommand.php
@@ -1,8 +1,27 @@
 <?php
-
 namespace App\Bot\Commands;
 
-class MetaCommand
-{
+use App\Bot\Interfaces\CommandInterface;
+use App\Bot\Services\TelegramService;
 
+class MetaCommand implements CommandInterface
+{
+    private TelegramService $telegramService;
+
+    public function __construct(TelegramService $telegramService)
+    {
+        $this->telegramService = $telegramService;
+    }
+
+    public function handle(array $message): void
+    {
+        $chatId = $message['chat']['id'];
+        $metaText = "Мета героев по ролям:\n" .
+            "<b>Роумер:</b> S+, S, A, B\n" .
+            "<b>Адк:</b> S+, A, B, C\n" .
+            "<b>Мидер:</b> S+, S, A\n" .
+            "<b>Эксп:</b> S+, A, B\n" .
+            "<b>Лесник:</b> S+, S, A, B";
+        $this->telegramService->sendMessage($chatId, $metaText);
+    }
 }
